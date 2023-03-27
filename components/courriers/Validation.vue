20917323a2cb9c05054e95fe9530e382a6c13787
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-03-27 23:17:06 +0000

    Update

--- components/courriers/Validation.vue
+++ components/courriers/Validation.vue
@@ -1,46 +1,62 @@
 <template>
     <div class="login-form-box registration-form">
         <h3 class="title"></h3>
-        <form class="row">
-            <div class="form-group col-6">
+        <form >
+            <div class="row border-grey">
+                <div class="form-group col-6">
                 <h5 for="reg-name">Nom*</h5>
                 <p>{{this.detailutilisateur && this.detailutilisateur.nom}}</p>
+                </div>
+                <div class="form-group col-6">
+                    <h5 for="reg-name">Prénom*</h5>
+                    <p>{{this.detailutilisateur && this.detailutilisateur.prenom}}</p>
+                </div>
+                <div class="form-group col-6">
+                    <h5 for="log-email">Email*</h5>
+                    <p>{{this.detailutilisateur && this.detailutilisateur.email}}</p>
+                </div>
+                <div class="form-group col-6">
+                    <h5 for="reg-name">Adresse*</h5>
+                    <p>{{this.detailutilisateur && this.detailutilisateur.adresse}}</p>
+                </div>
+                <div class="form-group col-6">
+                    <h5 for="reg-name">Téléphone*</h5>
+                    <p>{{this.detailutilisateur && this.detailutilisateur.telephone}}</p>
+                </div>
+                <div class="form-group col-3 justify-content-end">
+                    <button @click="$goToTab('coordonnees')" type="button" class="btn btn-outline-primary btn-medium float-end">Modifier <i class="icon-east"></i></button>
+                </div>
             </div>
-            <div class="form-group col-6">
-                <h5 for="reg-name">Prénom*</h5>
-                <p>{{this.detailutilisateur && this.detailutilisateur.prenom}}</p>
+            <div class="row border-grey">
+                <div class="form-group col-6">
+                    <h5 for="reg-name">Ministère</h5>
+                    <p>{{this.detailministere && this.detailministere.title}}</p>
+                </div>
+                <div class="form-group col-3 justify-content-end">
+                    <button @click="$goToTab('ministeres')" type="button" class="btn btn-outline-primary btn-medium float-end">Modifier <i class="icon-east"></i></button>
+                </div>
             </div>
-            <div class="form-group col-6">
-                <h5 for="log-email">Email*</h5>
-                <p>{{this.detailutilisateur && this.detailutilisateur.email}}</p>
+            <div class="row border-grey">
+                <div class="form-group col-6">
+                    <h5 for="reg-name">Objet*</h5>
+                    <p>{{this.detailcontenu && this.detailcontenu.objet}}</p>
+                </div>
+                <div class="form-group col-3 justify-content-end">
+                    <button @click="$goToTab('contenu')" type="button" class="btn btn-outline-primary btn-medium float-end">Modifier <i class="icon-east"></i></button>
+                </div>
+                <div class="form-group col-12">
+                    <h5 for="reg-name">Message*</h5>
+                    <p>{{this.detailcontenu && this.detailcontenu.message}}</p>
+                </div>
+                
             </div>
-            <div class="form-group col-6">
-                <h5 for="reg-name">Adresse*</h5>
-                <p>{{this.detailutilisateur && this.detailutilisateur.adresse}}</p>
-            </div>
-            <div class="form-group col-6">
-                <h5 for="reg-name">Téléphone*</h5>
-                <p>{{this.detailutilisateur && this.detailutilisateur.telephone}}</p>
-            </div>
-            <hr>
-            <div class="form-group col-12">
-                <h5 for="reg-name">Ministère</h5>
-                <p>{{this.detailministere && this.detailministere.title}}</p>
-            </div>
-            <hr>
-            <div class="form-group col-12">
-                <h5 for="reg-name">Objet*</h5>
-                <p>{{this.detailcontenu && this.detailcontenu.objet}}</p>
-            </div>
-            <div class="form-group col-12">
-                <h5 for="reg-name">Message*</h5>
-                <p>{{this.detailcontenu && this.detailcontenu.message}}</p>
-            </div>
-            <div class="form-group col-6">
-            <button @click="$goToTab('contenu')" type="button" class="edu-btn btn-medium"><i class="icon-west"></i> Précédent</button>
-            </div>
-            <div class="form-group col-6">
-                <button @click="submitValidation" type="button" class="edu-btn btn-medium">Confirmer <i class="icon-4"></i></button>
+            <div class="row border-grey">
+                <div class="form-group col-6">
+                <button @click="$goToTab('contenu')" type="button" class="edu-btn btn-medium"><i class="icon-west"></i> Précédent</button>
+                </div>
+                <div class="form-group col-6">
+                    <button @click="submitValidation" type="button" class="edu-btn btn-medium">Confirmer <i class="icon-4"></i></button>
+                </div>
             </div>
         </form>
     </div>
@@ -87,3 +103,10 @@ import { mapMutations, mapGetters } from 'vuex'
         },
     }
 </script>
+<style>
+.border-grey {
+  border: solid 2px #e8ecee;
+  padding: 25px;
+  margin-bottom: 10px;
+}
+</style>
